def457be16216a708c96236dec11980fd78d8a63
annavatar17 <gracey.bautista.hanasan@gmail.com>
2023-09-28 13:15:49 +0800

    * .gitignore: Initial Commit
    * ConsoleProject.sln:
    * Program.cs:
    * StringExtension.cs:
    * ConsoleProject.csproj:

---
+++ ConsoleProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the string to check or enter done to end: ");
+
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "done")
+                {
+                    break;
+                }
+                else
+                {
+                    var isMatch = input.IsMatchBrackets();
+                    Console.WriteLine($"Brackets matched?: {isMatch}\n");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleProject/StringExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleProject
+{
+    public static class StringExtension
+    {
+        public static bool IsMatchBrackets(this string str)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var character in str)
+            {
+                if (character == '<')
+                {
+                    stack.Push(character);
+                }
+                else if (stack.Count > 0 && character == '>')
+                {
+                    stack.Pop();
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
